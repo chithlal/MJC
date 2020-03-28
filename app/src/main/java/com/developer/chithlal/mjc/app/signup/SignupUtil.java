86ceb1cd4a37066e0224dcfd1ea329a40dee72b8
dword0 <dword0@gmail.com>
2020-03-28 11:19:04 +0530

    test1

--- app/src/main/java/com/developer/chithlal/mjc/app/signup/SignupUtil.java
+++ app/src/main/java/com/developer/chithlal/mjc/app/signup/SignupUtil.java
@@ -1,4 +1,51 @@
 package com.developer.chithlal.mjc.app.signup;
 
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+import static androidx.core.content.ContextCompat.startActivity;
+
 public class SignupUtil {
+
+    public void  updateUI(FirebaseUser account){
+        if(account != null){
+            //Toast.makeText(this,"U Signed In successfully",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this, /* NEW ACTIVITY IS SIGNED UP SUCCESSFULLY*/.class));
+        }else {
+            //Toast.makeText(this,"U Didnt signed in",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    public SignupUtil( String name, String email,String pwd, String ph){
+        mAuth.createUserWithEmailAndPassword(email,pwd).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(SignupActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+
+                // ...
+            }
+        });
+    }
 }
