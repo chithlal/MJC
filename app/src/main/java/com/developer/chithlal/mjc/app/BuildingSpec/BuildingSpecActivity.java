2a1a15260348d34cf066a7c15bb7872f96205c44
Codemac <devcodemac@gmail.com>
2020-04-05 19:00:45 +0530

    Build spec ui half Completed - chithlal

--- app/src/main/java/com/developer/chithlal/mjc/app/BuildingSpec/BuildingSpecActivity.java
+++ app/src/main/java/com/developer/chithlal/mjc/app/BuildingSpec/BuildingSpecActivity.java
@@ -1,6 +1,8 @@
 package com.developer.chithlal.mjc.app.BuildingSpec;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -8,11 +10,22 @@ import com.developer.chithlal.mjc.R;
 
 
 public class BuildingSpecActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_building_spec);
+        setContentView(R.layout.activity_building_specs);
+        mToolbar = findViewById(R.id.spec_toolbar);
 
+
+        //Toolbar setup
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        mToolbar.setTitle("");
+        mToolbar.setEnabled(true);
+        setSupportActionBar(mToolbar);
+        ActionBar mActionBar = getSupportActionBar();
+        if (mActionBar!=null)
+            mActionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
